835438d3581093e740bc04fa15ae8e3c6c3a0193
Shady Hussein <shadyhussein99@gmail.com>
2023-03-21 03:38:34 +0200

    Deploying

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ function Title() {
     return <div className=" mt-20 md:flex md:justify-center md:items-end" id="title">
 
         <div className="flex justify-center">
-            <img className=" rounded-full w-40 md:h-48" src="../pics/personal.jpg" alt="personal-pic" />
+            <img className=" rounded-full w-40 md:h-48" src="/pics/personal.jpg" alt="personal-pic" />
         </div>
 
         <div className="text-center mt-8 md:pl-12 md:text-left">
