bf1deb67edb249da0e2bb206a7c610021da81537
Shady Hussein <shadyhussein99@gmail.com>
2023-03-28 21:02:16 +0200

    Developing portfolio

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,14 +8,16 @@ function Technologies() {
         <h3 className="text-center text-xl font-semibold text-heading px-8 md:text-2xl">Here are some technologies I like working with:</h3>
 
         <div className="grid grid-cols-3 px-16 items-end md:grid-cols-4 md:mx-16 lg:mx-48 xl:mx-80">
-            <img className=" w-20 technologies-img md:w-24" src="/pics/react.png" alt="React" />
+            <img className=" w-20 technologies-img md:w-24" src="/pics/react.png" alt="React" />                  
             <img className=" w-20 technologies-img md:w-24" src="/pics/javascript.svg" alt="JavaScript" />
+            <img className=" w-24 technologies-img md:w-28" src="/pics/redux.png" alt="redux" />
             <img className=" w-24 technologies-img md:w-28" src="/pics/css3.svg" alt="CSS3" />
             <img className=" w-20 technologies-img md:w-24" src="/pics/html5.svg" alt="HTML5" />    
             <img className=" w-20 technologies-img md:w-24" src="/pics/material-ui.svg" alt="Material-UI" />
             <img className=" w-16 technologies-img md:w-20" src="/pics/bootstrap.svg" alt="Bootstrap" />
             <img className=" w-20 technologies-img md:w-24" src="/pics/tailwind.svg" alt="Tailwind" /> 
-            <img className=" w-20 technologies-img md:w-24" src="/pics/git.svg" alt="Git" />          
+            <img className=" w-20 technologies-img md:w-24" src="/pics/git.svg" alt="Git" />    
+            
         </div>
     </div>
 }
