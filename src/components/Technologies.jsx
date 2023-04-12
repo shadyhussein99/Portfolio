4654f99d686bee51790b6893fd28a2fdeba5a21c
Shady Hussein <shadyhussein99@gmail.com>
2023-04-12 11:42:00 +0200

    Developing portfolio

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -12,10 +12,10 @@ function Technologies() {
             <img className=" w-20 technologies-img md:w-24" src="/pics/javascript.svg" alt="JavaScript" />
             <img className=" w-24 technologies-img md:w-28" src="/pics/redux.png" alt="redux" />
             <img className=" w-24 technologies-img md:w-28" src="/pics/css3.svg" alt="CSS3" />
-            <img className=" w-20 technologies-img md:w-24" src="/pics/html5.svg" alt="HTML5" />    
-            <img className=" w-20 technologies-img md:w-24" src="/pics/material-ui.svg" alt="Material-UI" />
-            <img className=" w-16 technologies-img md:w-20" src="/pics/bootstrap.svg" alt="Bootstrap" />
+            <img className=" w-20 technologies-img md:w-24" src="/pics/html5.svg" alt="HTML5" />               
             <img className=" w-20 technologies-img md:w-24" src="/pics/tailwind.svg" alt="Tailwind" /> 
+            <img className=" w-16 technologies-img md:w-20 pb-2" src="/pics/bootstrap.svg" alt="Bootstrap" />
+            <img className=" w-20 technologies-img md:w-24" src="/pics/sass2.svg" alt="Sass" />
             <img className=" w-20 technologies-img md:w-24" src="/pics/git.svg" alt="Git" />    
             
         </div>
