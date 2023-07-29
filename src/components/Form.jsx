59fb5c7a8c56583d9e6f6292a1ad51ecba8859ba
Shady Hussein <shadyhussein99@gmail.com>
2023-07-29 17:48:49 +0300

    Developing Portfolio

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,23 +10,23 @@ function Form() {
         <form className="mt-12 md:mt-16" action="mailto:shadyhussein99@gmail.com" method="post">
 
             <section>
-                <section className="mt-10 flex justify-center">
-                    <label className="">Name:</label>
-                    <input className="text-black rounded-lg ml-4 pl-2 w-64 col-span-2" type="text" />
+                <section className="mt-10 grid grid-cols-2 ">
+                    <label className="col-span-1 text-right mr-28 ">Name:</label>
+                    <input className="col-span-1 text-black rounded-lg -ml-20 pl-2 w-64" type="text" />
                 </section>
 
-                <section className="mt-10 flex justify-center">
-                    <label className="">Email:</label>
-                    <input className="text-black rounded-lg ml-4 pl-2 w-64" type="email" />
+                <section className="mt-10 grid grid-cols-2 ">
+                    <label className="col-span-1 text-right mr-28 ">Email:</label>
+                    <input className="col-span-1 text-black rounded-lg -ml-20 pl-2 w-64" type="email" />
                 </section>
 
-                <section className="mt-10 flex justify-center">
-                    <label className="block">Message:</label>
-                    <textarea className=" text-black rounded-lg ml-2 pl-2 w-64 h-60" rows="10" cols="40" />
+                <section className="mt-10 grid grid-cols-2 ">
+                    <label className="col-span-1 text-right mr-28 ">Message:</label>
+                    <textarea className=" col-span-1 text-black rounded-lg -ml-20 pl-2 w-64 h-60" rows="10" cols="40" />
                 </section>
 
-                <section className="flex justify-center">
-                    <input className=" cursor-pointer w-64 bg-white mt-10 rounded-full py-2 text-black relative left-10 hover:bg-heading transition ease-out duration-300" type="submit" />
+                <section className=" flex justify-center">
+                    <button className=" w-64 bg-white mt-10 rounded-full py-2 text-black relative left-10 hover:bg-heading transition ease-out duration-300" type="submit">Submit</button>
                 </section>
             </section>
 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,5 @@
 // Title of portfolio
 
-import React from "react"
-
 function Title() {
     return <header className=" mt-20 md:flex md:justify-center md:items-end" id="title">
 
@@ -13,8 +11,8 @@ function Title() {
             <p>Hi, my name is</p>
             <h1 className=" text-heading text-2xl font-semibold py-3 md:text-3xl">Shady Hussein</h1>
             <p>I'm a frontend web developer at <span className="text-heading font-semibold">Blue Sun Automation</span></p>
-            <p>I like building useful projects</p>
-            <i><p className="font-semibold mt-6">You can find a sample of my work on Github</p></i>
+            <em><p className="font-semibold mt-6 mb-8">You can find a sample of my work on Github</p></em>
+            <a href="/pdf-files/Shady-Hussein-Frontend-Resume.pdf" download className=" bg-white text-black px-8 py-3 rounded-full hover:bg-heading transition ease-out duration-300">Download CV</a>
         </section>
 
     </header>
