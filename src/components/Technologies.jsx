e24520114afadf313deb1dbeef59b2f217142ef1
Shady Hussein <shadyhussein99@gmail.com>
2023-08-02 15:07:07 +0300

    Developing Portfolio

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -11,12 +11,15 @@ function Technologies() {
             <img className=" w-20 technologies-img md:w-24" src="/pics/react.png" alt="React-img" />                  
             <img className=" w-20 technologies-img md:w-24" src="/pics/javascript.svg" alt="JavaScript-img" />
             <img className=" w-20 technologies-img md:w-24" src="/pics/typescript.png" alt="TypeScript-img" />
-            <img className=" w-24 technologies-img md:w-28" src="/pics/redux.png" alt="R-imgedux" />
+            <img className=" w-24 technologies-img md:w-28" src="/pics/redux.png" alt="Redux-img" />
             <img className=" w-24 technologies-img md:w-28" src="/pics/css3.svg" alt="CSS3-img" />
             <img className=" w-20 technologies-img md:w-24" src="/pics/html5.svg" alt="HTML5-img" />               
             <img className=" w-20 technologies-img md:w-24" src="/pics/tailwind.svg" alt="Tailwind-img" /> 
             <img className=" w-16 technologies-img md:w-20 pb-2" src="/pics/bootstrap.svg" alt="Bootstrap-img" />
-            <img className=" w-20 technologies-img md:w-24" src="/pics/sass2.svg" alt="Sass-img" />
+            <img className=" w-20 technologies-img md:w-28" src="/pics/sass2.svg" alt="Sass-img" />
+            <img className=" w-20 technologies-img md:w-28" src="/pics/node.svg" alt="Node-img" />
+            <img className=" w-20 technologies-img md:w-24" src="/pics/express.svg" alt="Express-img" />
+            <img className=" w-20 technologies-img md:w-32" src="/pics/mongodb.svg" alt="MongoDB-img" />
             <img className=" w-20 technologies-img md:w-24" src="/pics/git.svg" alt="Git-img" />               
         </section>
     </section>
